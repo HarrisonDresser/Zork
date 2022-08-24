f3552c9d3aa3fe435c2146025e6d543a0b2f4c84
Harrison Dresser <65263567+HarrisonDresser@users.noreply.github.com>
2022-08-24 15:17:28 -0400

    Zork 1.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -2,11 +2,37 @@
 
 namespace Zork
 {
+
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            // string inputString = System.Console.ReadLine();
+            //inputString = inputString.ToUpper();
+
+            String inputString = Console.ReadLine().Trim().ToUpper();
+
+            //.Trim() does trailing and precedding spaces - also leftTrim() - and rightTrim()
+
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for Playing.");
+            }
+
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+            }
+
+            else
+            {
+                //$ is important to ensure the string value of inputString is displayed
+                Console.WriteLine($"Unrecognized commad: {inputString}");
+            }
+
         }
     }
 }
