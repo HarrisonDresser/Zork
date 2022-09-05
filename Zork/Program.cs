735b3cbdd6de4649a081c68158107eb95e245e60
HarrisonDresser <harry.dresser777@gmail.com>
2022-09-05 15:13:14 -0400

    Update Program.cs
    Zork 1.1 - Looking Around and Quitting

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -2,35 +2,25 @@
 
 namespace Zork
 {
-
-
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
-            // string inputString = System.Console.ReadLine();
-            //inputString = inputString.ToUpper();
-
-            String inputString = Console.ReadLine().Trim().ToUpper();
-
-            //.Trim() does trailing and precedding spaces - also leftTrim() - and rightTrim()
-
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
             if (inputString == "QUIT")
             {
                 Console.WriteLine("Thank you for Playing.");
             }
-
             else if (inputString == "LOOK")
             {
                 Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
             }
-
             else
             {
-                //$ is important to ensure the string value of inputString is displayed
-                Console.WriteLine($"Unrecognized commad: {inputString}");
+                Console.WriteLine("Unrecognized command.");
             }
 
         }
