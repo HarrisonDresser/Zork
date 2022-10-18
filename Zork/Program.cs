73831dde336331d808062306ab0b2da27a1ebae5
HarrisonDresser <harry.dresser777@gmail.com>
2022-10-17 23:16:59 -0400

    Zork 4.3
    A Larger World

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,140 +8,21 @@ namespace Zork
 
     class Program
     {
-        private static Room CurrentRoom
+        static void Main(string[] args)
         {
-            get
-            {
-                return _rooms[_location.Row, _location.Column];
-            }
-        }
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
 
-        private enum Fields
-        {
-            Name = 0,
-            Description
+            Game game = Game.Load(gameFilename);
+            Console.WriteLine("Welcom to Zork!");
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
+
         }
 
         private enum CommandLineArguments
         {
-            RoomsFilename = 0
+            GameFilename = 0
         }
-
-
-        static void Main(string[] args)
-        {
-
-            const string defaultRoomsFilename = "Rooms.json";
-            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
-            InitializeRooms(roomsFilename);
-
-            Console.WriteLine("Welcome to Zork!");
-
-            Room previousRoom = null;
-            Commands command = Commands.UNKNOWN;
-            while (command != Commands.QUIT)
-            {
-                Console.WriteLine(CurrentRoom);
-                if (previousRoom != CurrentRoom)
-                {
-                    Console.WriteLine(CurrentRoom.Description);
-                    previousRoom = CurrentRoom;
-                }
-
-                Console.Write("> ");
-
-                command = ToCommand(Console.ReadLine().Trim());
-
-                switch (command)
-                {
-                    case Commands.QUIT:
-                        Console.WriteLine("Thank you for playing!");
-                        break;
-
-                    case Commands.LOOK:
-                        Console.WriteLine(CurrentRoom.Description);
-                        break;
-
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        if (Move(command) == false)
-                        {
-                            Console.WriteLine("The way is shut");
-                        }
-
-                        break;
-
-                    default:
-                        Console.WriteLine("Unknown command.");
-                        break;
-
-                }
-            }
-        }
-
-        private static bool Move(Commands command)
-        {
-            Assert.IsTrue(IsDirection(command), " Invalid direction.");
-
-            bool didMove;
-
-            switch (command)
-            {
-                case Commands.NORTH when _location.Row < _rooms.GetLength(0) - 1:
-                    _location.Row++;
-                    didMove = true;
-                    break;
-                case Commands.SOUTH when _location.Row > 0:
-                    _location.Row--;
-                    didMove = true;
-                    break;
-
-                case Commands.EAST when _location.Column < _rooms.GetLength(1) - 1:
-                    _location.Column++;
-                    didMove = true;
-                    break;
-
-                case Commands.WEST when _location.Column > 0:
-                    _location.Column--;
-                    didMove = true;
-                    break;
-
-                default:
-                    didMove = false;
-                    break;
-            }
-            return didMove;
-        }
-
-        private static void InitializeRooms(string roomsFilename) =>
-             _rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
-       
-        
-
-        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
-
-        private static bool IsDirection(Commands command) => Directions.Contains(command);
-
-        private static Room[,] _rooms = 
-        {
-            { new Room ("Rocky Trail"),new Room ("South of House"),new Room ("Canyon View") },
-            { new Room ("Forest"), new Room ("West of House"),new Room ("Behind House") },
-            { new Room ("Dense Woods"), new Room ("North of House"), new Room ("Clearing") }
-        };
-
-        private static readonly List<Commands> Directions = new List<Commands>
-        {
-            Commands.NORTH,
-            Commands.SOUTH,
-            Commands.EAST,
-            Commands.WEST
-        };
-
-          private static (int Row, int Column) _location = (1, 1);
-
     }
-
-
 }
---
+++ Zork/World.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.Serialization;
+using System.Net.Http;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace Zork
+{
+    public class World
+    {
+        public HashSet<Room> Rooms { get; set; }
+
+        [JsonIgnore]
+        public IReadOnlyDictionary<string, Room> RoomsByName => mRoomsByName;
+
+
+        public Player SpawnPlayer() => new Player(this, StartingLocation);
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            mRoomsByName = Rooms.ToDictionary(room => room.Name, room => room);
+            foreach (Room room in Rooms)
+            {
+                room.UpdateNeighbors(this);
+                
+            }
+            
+        }
+
+        [JsonProperty]
+        private string StartingLocation { get; set; }
+
+        private Dictionary<string, Room> mRoomsByName;
+
+
+
+        //public Room[] Rooms { get; }
+
+        //public Dictionary<string, Room> RoomsByName { get; }
+
+       // public World(Room[] rooms)
+       // {
+        //    Rooms = rooms;
+        //}
+
+      
+    }
+}
